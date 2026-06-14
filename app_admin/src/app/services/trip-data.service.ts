6cca0f85acc5e9708565411eff63eea6d63e1101
Nicholas Deniz <nicholas.deniz@snhu.com>
2026-06-13 23:02:22 -0400

    Module 6 completed baseline

---
+++ app_admin/src/app/services/trip-data.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Trip } from '../models/trip';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TripDataService {
+    constructor(private http: HttpClient) {}
+
+    url = 'http://localhost:3000/api/trips';
+
+    getTrip(tripCode: string): Observable<Trip[]> {
+        return this.http.get<Trip[]>(this.url + '/' + tripCode);
+    }
+
+    addTrip(formData: Trip): Observable<Trip> {
+        return this.http.post<Trip>(this.url, formData);
+    }
+
+    updateTrip(formData: Trip): Observable<Trip> {
+        return this.http.put<Trip>(this.url + '/' + formData.code, formData);
+    }
+
+    getTrips(): Observable<Trip[]> {
+        return this.http.get<Trip[]>(this.url);
+    }
+}
---
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TripCardComponent } from '../trip-card/trip-card.component';
+import { Trip } from '../models/trip';
+import { TripDataService } from '../services/trip-data.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-trip-listing',
+  standalone: true,
+  imports: [CommonModule, TripCardComponent],
+  templateUrl: './trip-listing.component.html',
+  styleUrl: './trip-listing.component.css',
+  providers: [TripDataService]
+})
+export class TripListingComponent implements OnInit {
+  trips: Trip[] = [];
+  message: string = '';
+
+  constructor(
+    private tripDataService: TripDataService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {
+    console.log('trip-listing constructor');
+  }
+
+  public addTrip(): void {
+    this.router.navigate(['/add-trip']);
+  }
+
+  private getStuff(): void {
+    this.tripDataService.getTrips()
+      .subscribe({
+        next: (value: Trip[]) => {
+          this.trips = value;
+
+          if (value.length > 0) {
+            this.message = 'There are ' + value.length + ' trips available.';
+          } else {
+            this.message = 'There were no trips retrieved from the database';
+          }
+
+          console.log(this.message);
+          this.cdr.detectChanges();
+        },
+        error: (error: any) => {
+          this.message = 'An error occurred while retrieving trips from the database.';
+          console.log('Error: ' + error);
+          this.cdr.detectChanges();
+        }
+      });
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.getStuff();
+  }
+}
